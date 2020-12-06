653452f93eebcba891458f4969baaa4112243837
Damien Rochford <djrochford@gmail.com>
2020-12-05 20:14:39 -0600

    Add `_calculate_probabilities` function and tests; fix some type annotations

--- ngram/ngram.py
+++ ngram/ngram.py
@@ -3,11 +3,12 @@ Ngram language model class and functions that operate on it.
 """
 import re
 from collections import defaultdict
-from typing import Dict, List, Tuple
+from typing import DefaultDict, List, Tuple
 
-from nltk import sent_tokenize, word_tokenize
+from nltk import sent_tokenize, word_tokenize # type: ignore
 
-NgramContainer = List[Dict[Tuple[str, ...], float]]
+CountObject = List[DefaultDict[Tuple[str, ...], int]]
+NgramContainer = List[DefaultDict[Tuple[str, ...], float]]
 
 class NgramModel:
     """
@@ -15,7 +16,7 @@ class NgramModel:
     """
 
     def __init__(self, training_text: str, order: int):
-        self._ngrams = _count(training_text, order)
+        self._ngrams = _calculate_probabilities(_count(training_text, order))
         self._order = order
 
     def __getitem__(self, key):
@@ -43,11 +44,11 @@ class NgramModel:
 BEGIN_TOKEN = "<s>"
 END_TOKEN = "</s>"
 
-def _count(text: str, order: int) -> NgramContainer:
+def _count(text: str, order: int) -> CountObject:
     """
     Return counts of all n-grams of order `order` and below.
     """
-    counts = [defaultdict(int) for _ in range(order)]
+    counts: CountObject = [defaultdict(int) for _ in range(order)]
     assert not re.search(f"{re.escape(BEGIN_TOKEN)}|{re.escape(END_TOKEN)}", text), \
            f"Text cannot contain substrings '{BEGIN_TOKEN}'' or '{END_TOKEN}'."
     for sentence in sent_tokenize(text):
@@ -56,7 +57,20 @@ def _count(text: str, order: int) -> NgramContainer:
         for i in range(words_length):
             for j in range(order):
                 if i + j + 1 <= words_length:
-                    jgram = tuple(words[i:i+j+1])
-                    if jgram != (BEGIN_TOKEN,):
-                        counts[j][jgram] += 1
+                    counts[j][tuple(words[i:i+j+1])] += 1
     return counts
+
+def _calculate_probabilities(counts: CountObject) -> NgramContainer:
+    probabilities: NgramContainer = [defaultdict(float) for _ in range(len(counts))]
+    unigrams = counts[0]
+    total_unigrams = 0
+    for number in unigrams.values():
+        total_unigrams += number
+    for i, ngram_count in enumerate(counts):
+        for ngram in ngram_count:
+            if i == 0:
+                denominator = total_unigrams
+            else:
+                denominator = counts[i-1][ngram[:-1]]
+            probabilities[i][ngram] = ngram_count[ngram] / denominator
+    return probabilities
--- ngram/test_ngram.py
+++ ngram/test_ngram.py
@@ -4,30 +4,30 @@ Tests for the NGramModel class
 
 from unittest import TestCase
 
-from ngram import BEGIN_TOKEN, END_TOKEN, _count
+from ngram import BEGIN_TOKEN, END_TOKEN, _calculate_probabilities, _count
+
+SIMPLE_CORPUS = "I am Sam. Sam I am. I do not like green eggs and ham."
 
 class TestCount(TestCase):
     """
     Tests for `_count` function.
     """
-    def setUp(self):
-        self.simple_corpus = "I am Sam. Sam I am. I do not like green eggs and ham."
 
     def test_unigram(self, counts=None):
         """
         Test we get correct unigram counts on a simple corpus.
         """
         if not counts:
-            counts = _count(self.simple_corpus, 1)
+            counts = _count(SIMPLE_CORPUS, 1)
             self.assertEqual(len(counts), 1)
         unigrams = counts[0]
-        self.assertEqual(len(unigrams), 12)
-        expected_grams = {("I",), ("am",), ("Sam",), (".",), (END_TOKEN,), ("do",), ("not",),
-                          ("like",), ("green",), ("eggs",), ("and",), ("ham",)}
+        self.assertEqual(len(unigrams), 13)
+        expected_grams = {(BEGIN_TOKEN,), ("I",), ("am",), ("Sam",), (".",), (END_TOKEN,), ("do",),
+                          ("not",), ("like",), ("green",), ("eggs",), ("and",), ("ham",)}
         self.assertEqual(set(unigrams.keys()), expected_grams)
         for gram in expected_grams:
             count = unigrams[gram]
-            if gram in [("I",), (".",), (END_TOKEN,)]:
+            if gram in [(BEGIN_TOKEN,), ("I",), (".",), (END_TOKEN,)]:
                 self.assertEqual(count, 3)
             elif gram in [("am",), ("Sam",)]:
                 self.assertEqual(count, 2)
@@ -39,7 +39,7 @@ class TestCount(TestCase):
         Test we get correct bigram and unigram counts on a simple corpus.
         """
         if not counts:
-            counts = _count(self.simple_corpus, 2)
+            counts = _count(SIMPLE_CORPUS, 2)
             self.assertEqual(len(counts), 2)
         bigrams = counts[1]
         self.assertEqual(len(bigrams), 16)
@@ -62,7 +62,7 @@ class TestCount(TestCase):
         """
         Test we get correct trigram, bigram and unigram counts on a simple corpus.
         """
-        counts = _count(self.simple_corpus, 3)
+        counts = _count(SIMPLE_CORPUS, 3)
         self.assertEqual(len(counts), 3)
         trigrams = counts[2]
         self.assertEqual(len(trigrams), 17)
@@ -77,3 +77,80 @@ class TestCount(TestCase):
             count = trigrams[gram]
             self.assertEqual(count, 1)
         self.test_bigram(counts)
+
+class TestCalcProb(TestCase):
+    """
+    Tests for `_calculate_probabilities` function.
+    """
+
+    def test_unigram(self, probs=None):
+        """
+        Test we get accurate (asbolute) probabilities of unigrams of simple corpus.
+        """
+        if not probs:
+            probs = _calculate_probabilities(_count(SIMPLE_CORPUS, 1))
+            self.assertEqual(len(probs), 1)
+        unigram_probs = probs[0]
+        self.assertEqual(len(unigram_probs), 13)
+        expected_grams = {(BEGIN_TOKEN,), ("I",), ("am",), ("Sam",), (".",), (END_TOKEN,), ("do",), ("not",),
+                          ("like",), ("green",), ("eggs",), ("and",), ("ham",)}
+        self.assertEqual(set(unigram_probs.keys()), expected_grams)
+        for gram in expected_grams:
+            prob = unigram_probs[gram]
+            if gram in [(BEGIN_TOKEN,), ("I",), (".",), (END_TOKEN,)]:
+                self.assertEqual(prob, 3/23)
+            elif gram in [("am",), ("Sam",)]:
+                self.assertEqual(prob, 2/23)
+            else:
+                self.assertEqual(prob, 1/23)
+
+    def test_bigram(self, probs=None):
+        """
+        Test we get correct (conditional) bigram and (absolute) unigram probabilities on a simple corpus.
+        """
+        if not probs:
+            probs = _calculate_probabilities(_count(SIMPLE_CORPUS, 2))
+            self.assertEqual(len(probs), 2)
+        bigram_probs = probs[1]
+        self.assertEqual(len(bigram_probs), 16)
+        expected_grams = {(BEGIN_TOKEN, "I"), ("I", "am"), ("am", "Sam"), ("Sam", "."),
+                          (".", END_TOKEN), (BEGIN_TOKEN, "Sam"), ("Sam", "I"), ("am", "."),
+                          ("I", "do"), ("do", "not"), ("not", "like"), ("like", "green"),
+                          ("green", "eggs"), ("eggs", "and"), ("and", "ham"), ("ham", ".")}
+        self.assertEqual(set(bigram_probs.keys()), expected_grams)
+        for gram in expected_grams:
+            prob = bigram_probs[gram]
+            if gram in [(BEGIN_TOKEN, "I"), ("I", "am")]:
+                self.assertEqual(prob, 2/3)
+            elif gram in [("am", "Sam"), ("Sam", "."), ("Sam", "I"), ("am", ".")]:
+                self.assertEqual(prob, 1/2)
+            elif gram in [(BEGIN_TOKEN, "Sam"), ("I", "do")]:
+                self.assertEqual(prob, 1/3)
+            else:
+                self.assertEqual(prob, 1)
+        self.test_unigram(probs)
+
+    def test_trigram(self, probs=None):
+        """
+        Test we get correct (conditional) bigram and (absolute) unigram probabilities on a simple corpus.
+        """
+        if not probs:
+            probs = _calculate_probabilities(_count(SIMPLE_CORPUS, 3))
+            self.assertEqual(len(probs), 3)
+        trigram_probs = probs[2]
+        self.assertEqual(len(trigram_probs), 17)
+        expected_grams = {(BEGIN_TOKEN, "I", "am"), ("I", "am", "Sam"), ("am", "Sam", "."),
+                          ("Sam", ".", END_TOKEN), (BEGIN_TOKEN, "Sam", "I"), ("Sam", "I", "am"),
+                          ("I", "am", "."), ("am", ".", END_TOKEN), (BEGIN_TOKEN, "I", "do"),
+                          ("I", "do", "not"), ("do", "not", "like"), ("not", "like", "green"),
+                          ("like", "green", "eggs"), ("green", "eggs", "and"),
+                          ("eggs", "and", "ham"), ("and", "ham", "."), ("ham", ".", END_TOKEN)}
+        self.assertEqual(set(trigram_probs.keys()), expected_grams)
+        for gram in expected_grams:
+            prob = trigram_probs[gram]
+            if gram in [(BEGIN_TOKEN, "I", "am"), ("I", "am", "Sam"), ("I", "am", "."),
+                        (BEGIN_TOKEN, "I", "do")]:
+                self.assertEqual(prob, 1/2)
+            else:
+                self.assertEqual(prob, 1)
+        self.test_bigram(probs)
